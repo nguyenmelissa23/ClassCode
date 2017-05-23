fa961ae468d0ef07711645efdd3d95328dddf71f
Melissa Nguyen <nguyen.melissa23@gmail.com>
2017-05-23 11:28:26 -0700

    19.1

---
+++ 19.1 Intro to ReactJS/HelloReact/app.js
@@ -0,0 +1,10 @@
+// Include the Main React Dependencies 
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// This code renders a simple "Hello World"
+
+
+ReactDOM.render(                            // This is React 
+    <p>Hello World!</p>,                    // JSX treat as string without " "
+    document.getElementById("app"));
