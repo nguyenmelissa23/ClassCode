08ce4d47ac549b84faae0dea42d2901ae4086f1e
Melissa Nguyen <nguyen.melissa23@gmail.com>
2017-06-15 09:29:13 -0700

    22.2 React Native

---
+++ 22.2 React Native/touchables/searchResults/index.ios.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import SearchResults from './SearchResults';
+
+export default class searchResults extends Component {
+  render() {
+    return (
+		<View>
+			<SearchResults/>
+		</View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('searchResults', () => searchResults);
